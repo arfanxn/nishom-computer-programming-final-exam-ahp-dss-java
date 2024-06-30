dbf85c08db3a2114c6746c5effb8111f20d518c4
arfanxn <arfan2173@gmail.com>
2024-07-01 00:56:58 +0700

    made user model

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,24 +1,51 @@
 package models;
 
-import java.util.HashMap;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author arfanxn
  */
-public class User { 
+@JsonIgnoreProperties({"provider", "provider_id", "provider_token", "token"})
+public class User {
+
+    private String id;
+    private String name;
+    private String email;
+    @JsonProperty("email_verified_at")
+    private String emailVerifiedAt;
+    private String password;
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("created_at")
+    private String createdAt;
+    @JsonProperty("updated_at")
+    private String updatedAt;
     
-    HashMap<String, String> credentials = new HashMap<>();
-    
-    private String email, password;
-    
+
     public User() {
-        System.out.println(this.credentials);
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -29,6 +56,14 @@ public class User {
         this.email = email;
     }
 
+    public String getEmailVerifiedAt() {
+        return emailVerifiedAt;
+    }
+
+    public void setEmailVerifiedAt(String emailVerifiedAt) {
+        this.emailVerifiedAt = emailVerifiedAt;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -36,5 +71,28 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }  
+
 }
